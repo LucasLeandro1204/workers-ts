c45e35ca24938c97a17ece0c0cfadfa767adbb2e
LucasLeandro1204 <lucasleandro1204@gmail.com>
2023-05-05 12:47:44 -0300

    chore: wip

--- src/worker.ts
+++ src/worker.ts
@@ -6,11 +6,22 @@ import { createFlowRouter } from './api/flow';
 export function createWorker() {
   const app = new Hono<HonoInterface>();
 
+  app.use('*', cors({
+    origin: 'http://localhost:8923',
+    allowMethods: ['POST', 'HEAD', 'GET', 'OPTIONS'],
+    credentials: true,
+    allowHeaders: ['Teams-Enabled', 'cookies', 'cookie', 'Cookie', 'set-cookie', 'Time-Zone', 'Sent-By', 'Triggered-By', 'twProjectsVer', 'Accept', 'X-Requested-With'],
+  }));
   app.use('*', prettyJSON());
-  app.use('*', cors({ origin: '*' }));
   app.get('/', (ctx) => ctx.text('Worker is running.'));
 
   app.route('/flow', createFlowRouter());
+  // app.options('/proxy', (ctx) => ctx.text('ok'));
+  app.all('/proxy', (ctx) => {
+    console.dir(JSON.stringify([...ctx.req.headers.entries()], null, 2));
+    console.dir(ctx.req.cookie());
+    return ctx.json({ success: true });
+  });
 
   app.notFound((ctx) =>
     ctx.json(
